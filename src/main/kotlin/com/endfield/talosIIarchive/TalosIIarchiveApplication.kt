3b07cca7a4cecf2a2fe110b8d7c322c965cb3043
Kriol368 <kriol368@gmail.com>
2026-01-26 16:10:14 +0100

    Initial commit

---
+++ src/main/kotlin/com/endfield/talosIIarchive/TalosIIarchiveApplication.kt
@@ -0,0 +1,11 @@
+package com.endfield.talosIIarchive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TalosIIarchiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<TalosIIarchiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/endfield/talosIIarchive/TalosIIarchiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.endfield.talosIIarchive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TalosIIarchiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
